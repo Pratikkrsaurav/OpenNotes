28891e8242450b6ee688468238c24ed369bd4913
Pratik <pratikkrsaurav199@gmail.com>
2026-02-16 17:26:54 +0530

    Add Hero component to Home page

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,9 +1,12 @@
+import Hero from '../components/Hero'
+import { Button } from '../components/ui/button'
 import React from 'react'
+
 
 const Home = () => {
   return (
-    <div>
-      home hai bhai
+    <div className='pt-20'>
+      <Hero />
     </div>
   )
 }
